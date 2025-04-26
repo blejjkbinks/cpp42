e2ab4518345dd31f877b1764aa71dc85e573a3e9
debian blejj <debian_utm@blejj>
2025-04-26 11:56:46 +0930

    fclean_all

--- fclean_all.sh
+++ fclean_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo -n > making
+
 find . -type f -name Makefile | while read makefile; do
 	dirname=$(dirname "$makefile")
 	echo "Running fclean in: $dirname"
+	if ! make -C "$dirname" re; then
+		echo "$dirname" >> making
+	fi
 	make -C "$dirname" fclean
 done
